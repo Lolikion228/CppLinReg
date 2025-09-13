6c9e4fd9a670482e1b17c2d1d084adb8cb663b83
Lolikion228 <st117139@student.spbu.ru>
2025-09-13 12:26:12 +0300

    change lr to exp

--- LinearRegression/main.cpp
+++ LinearRegression/main.cpp
@@ -8,33 +8,29 @@ int main(int argc, char**args){
     
     int n_epochs;
     double initial_lr;
-    double min_lr;
-    int T;
+    double decay_rate;
     std::string fpath;
 
     if(argc==1){
         n_epochs = 512;
-        initial_lr = 1.0;
-        min_lr = 1e-7;
-        T = 64;
+        initial_lr = 0.01;
+        decay_rate = 0.25;
         fpath = "./data/data.txt";
     }
-    else if(argc == 6) {
+    else if(argc == 5) {
         n_epochs = std::stoi(args[1]);
         initial_lr = std::stod(args[2]);
-        min_lr = std::stod(args[3]);
-        T = std::stoi(args[4]);
-        fpath = args[5];
+        decay_rate = std::stod(args[3]);
+        fpath = args[4];
     }
     else{
         throw std::invalid_argument("you should pass 0 or 5 arguments, you passed " + std::to_string(argc-1) + "." );
         return 1;
     }
 
-    CosineDecay* lr = new CosineDecay(initial_lr, min_lr, T);
+    ExponentialDecay* lr = new ExponentialDecay(initial_lr, decay_rate);
 
     auto [X, y, dim, n_obj] = process_data(fpath);
-
 
     LinReg LR1{dim, -0.1, 0.1};
 
